80d028202709d999ed59764e317bf1d6d665792c
jjozic <jjozic98@gmail.com>
2020-05-25 17:45:18 +0200

    Made random word generator work
    - implemented functionality with running the method recursivly

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 var words = ['hello', 'how', 'house', 'boy', 'girl', 'planet']
 var randomWord = words[Math.floor(Math.random()*words.length)]
-var run = true
-while(run){
-    //setTimeout(console.log(randomWord), 500)
+var run = 0
+
+setTimeout(() => generateRandomWord(), 200)
+
+function generateRandomWord(){
+    console.log(randomWordGenerator())
+    var text = document.getElementById('gametext')
+    text.innerHTML += (' ' + randomWordGenerator())
+    run++
+    if(run<40) setTimeout(() => generateRandomWord(), 200)
+}
+
+function randomWordGenerator(){
+    return words[Math.floor(Math.random()*words.length)]
 }
