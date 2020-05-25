4f2db7a84e5b1a8e88afb7385ce6f702818ef739
jjozic <jjozic98@gmail.com>
2020-05-26 01:58:05 +0200

    Added a reset button
    - added reset button, to restart the game
    - fixed a typo

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ document.addEventListener('DOMContentLoaded', () => {
     var words = ['hello', 'how', 'house', 'boy', 'girl', 'planet']
     var randomWord = words[Math.floor(Math.random() * words.length)]
     var run = 0
+    var text = document.getElementById('game-text')
 
     setTimeout(() => generateRandomWord(), 500)
 
     function generateRandomWord() {
         console.log(randomWordGenerator())
-        var text = document.getElementById('game-text')
         text.innerHTML += (' ' + randomWordGenerator())
         run++
         if (run < 40) setTimeout(() => generateRandomWord(), 500)
@@ -15,5 +15,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function randomWordGenerator() {
         return words[Math.floor(Math.random() * words.length)]
+    }
+
+    function reset(){
+        text.innerHTML = 'start'
+        console.log('gurke to go')
     }
 })
