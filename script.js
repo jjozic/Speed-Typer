5927d44bdf968ba55c79b17cf803ae223b9a1ae3
jjozic <jjozic98@gmail.com>
2020-05-25 17:26:09 +0200

    Added first elements and word generator
    - simple layout with a text display and a text area
    - create random words from an array of strings with help of Math.Random()

---
+++ script.js
@@ -0,0 +1,6 @@
+var words = ['hello', 'how', 'house', 'boy', 'girl', 'planet']
+var randomWord = words[Math.floor(Math.random()*words.length)]
+var run = true
+while(run){
+    //setTimeout(console.log(randomWord), 500)
+}
