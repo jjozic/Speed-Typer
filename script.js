149ca8af08ed7dcfbd4bb611de1ecca9b4ac5813
jjozic <jjozic98@gmail.com>
2020-05-31 18:30:56 +0200

    Added ids and made timer work

--- script.js
+++ script.js
@@ -3,14 +3,18 @@ document.addEventListener('DOMContentLoaded', () => {
     var randomWord = words[Math.floor(Math.random() * words.length)]
     var run = 0
     var text = document.getElementById('game-text')
+    var time = document.getElementById('time')
 
     setTimeout(() => generateRandomWord(), 500)
 
     function generateRandomWord() {
         console.log(randomWordGenerator())
         text.innerHTML += (' ' + randomWordGenerator())
+        time.innerHTML = 'Time: ' + (run/2) + 's';
         run++
-        if (run < 40) setTimeout(() => generateRandomWord(), 500)
+        if (run < 40) {
+            setTimeout(() => generateRandomWord(), 500)
+        }
     }
 
     function randomWordGenerator() {
