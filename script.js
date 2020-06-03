4b07e756484f916627093d030042dd6b7f3c60ce
Julian Jozic <jjozic98@gmail.com>
2020-06-03 15:30:46 +0200

    Reset button and working on comparison

--- script.js
+++ script.js
@@ -1,27 +1,54 @@
+function reset() {
+    console.log('gurke to go')
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     var words = ['hello', 'how', 'house', 'boy', 'girl', 'planet']
     var randomWord = words[Math.floor(Math.random() * words.length)]
     var run = 0
-    var text = document.getElementById('game-text')
+    const text = document.getElementById('textDisplay')
     var time = document.getElementById('time')
-    var typingBox = document.getElementById('typing-box')
+    var typingBox = document.getElementById('typingDisplay')
     var scoreLabel = document.getElementById('score')
 
     setTimeout(() => generateRandomWord(), 500)
 
     //reacts on each input of the typing box
     typingBox.addEventListener('input', () => {
-        let score=0
+        let score = 0
+        const arrayQuote = text.querySelectorAll('span')
         const arrayValue = typingBox.value.split('')
 
         console.log(arrayValue)
 
-        for (let i = 0; i < arrayValue.length; i++) {
+        /*for (let i = 0; i < arrayValue.length; i++) {
             if (arrayValue[i] === text.innerHTML[i]) {
                 score++
             }
         }
-        scoreLabel.innerHTML = score;
+        scoreLabel.innerHTML = score;*/
+
+
+        let correct = true
+        arrayQuote.forEach((characterSpan, index) => {
+            const character = arrayValue[index]
+            const span = characterSpan.innerText
+            const blub = 0
+            if (character == null) {
+                characterSpan.classList.remove('correct')
+                characterSpan.classList.remove('incorrect')
+                correct = false
+            } else if (character === characterSpan.innerText) {
+                characterSpan.classList.add('correct')
+                characterSpan.classList.remove('incorrect')
+            } else {
+                characterSpan.classList.remove('correct')
+                characterSpan.classList.add('incorrect')
+                correct = false
+            }
+        })
+
+        //if (correct) renderNewQuote()
     })
 
     function generateRandomWord() {
@@ -38,8 +65,5 @@ document.addEventListener('DOMContentLoaded', () => {
         return words[Math.floor(Math.random() * words.length)]
     }
 
-    function reset() {
-        text.innerHTML = 'start'
-        console.log('gurke to go')
-    }
+  
 })
