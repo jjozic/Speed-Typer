3c8e0fea057e4fab4f5322896e9f08ec02acbbee
Julian Jozic <jjozic98@gmail.com>
2020-06-03 17:55:13 +0200

    Added random quote, proper comparison
    - inserts random quote from an api call into the textbox
    - added proper timer in seconds
    - score counts letters for each quote
    - added WPM field (doesn't work yet)

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const RANDOM_QUOTE_API_URL = 'http://api.quotable.io/random'
+
 function reset() {
     console.log('gurke to go')
 }
@@ -6,17 +8,18 @@ document.addEventListener('DOMContentLoaded', () => {
     var words = ['hello', 'how', 'house', 'boy', 'girl', 'planet']
     var randomWord = words[Math.floor(Math.random() * words.length)]
     var run = 0
-    const text = document.getElementById('textDisplay')
-    var time = document.getElementById('time')
-    var typingBox = document.getElementById('typingDisplay')
-    var scoreLabel = document.getElementById('score')
+    const quoteBox = document.getElementById('textDisplay')
+    const timerElement  = document.getElementById('timer')
+    const typingBox = document.getElementById('typingDisplay')
+    const scoreLabel = document.getElementById('score')
+    const wpmLabel = document.getElementById('wpm')
 
-    setTimeout(() => generateRandomWord(), 500)
+    renderNewQuote(true)
 
     //reacts on each input of the typing box
     typingBox.addEventListener('input', () => {
         let score = 0
-        const arrayQuote = text.querySelectorAll('span')
+        const arrayQuote = quoteBox.querySelectorAll('span')
         const arrayValue = typingBox.value.split('')
 
         console.log(arrayValue)
@@ -28,12 +31,9 @@ document.addEventListener('DOMContentLoaded', () => {
         }
         scoreLabel.innerHTML = score;*/
 
-
         let correct = true
         arrayQuote.forEach((characterSpan, index) => {
             const character = arrayValue[index]
-            const span = characterSpan.innerText
-            const blub = 0
             if (character == null) {
                 characterSpan.classList.remove('correct')
                 characterSpan.classList.remove('incorrect')
@@ -41,29 +41,50 @@ document.addEventListener('DOMContentLoaded', () => {
             } else if (character === characterSpan.innerText) {
                 characterSpan.classList.add('correct')
                 characterSpan.classList.remove('incorrect')
+                score++
             } else {
                 characterSpan.classList.remove('correct')
                 characterSpan.classList.add('incorrect')
                 correct = false
+                score--
             }
         })
-
-        //if (correct) renderNewQuote()
+        scoreLabel.innerHTML = score
+        wpmLabel.innerHTML = getTimerTime()/score
+        if (correct) renderNewQuote()
     })
 
-    function generateRandomWord() {
-        text.innerHTML += (' ' + randomWordGenerator())
-        time.innerHTML = 'Time: ' + (run) + 's';
-        console.log(typingBox.innerHTML)
-        run++
-        if (run < 40) {
-            setTimeout(() => generateRandomWord(), 1000)
+    function getRandomQuote() {
+        return fetch(RANDOM_QUOTE_API_URL)
+            .then(response => response.json())
+            .then(data => data.content)
+    }
+
+    async function renderNewQuote(newPage) {
+        const quote = await getRandomQuote()
+        quoteBox.innerHTML = ''
+        quote.split('').forEach(character => {
+            const characterSpan = document.createElement('span')
+            characterSpan.innerText = character
+            quoteBox.appendChild(characterSpan)
+        })
+        typingBox.value = null
+        if(!newPage){
+            return
         }
+        startTimer()
     }
 
-    function randomWordGenerator() {
-        return words[Math.floor(Math.random() * words.length)]
+    let startTime
+    function startTimer() {
+        timerElement.innerText = 'Time: 0s'
+        startTime = new Date()
+        setInterval(() => {
+            timer.innerText = 'Time: ' + getTimerTime() + 's'
+        }, 1000)
     }
 
-  
+    function getTimerTime() {
+        return Math.floor((new Date() - startTime) / 1000)
+      }
 })
