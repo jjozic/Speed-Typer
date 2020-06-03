f53037126ac1f499a5c3d2648976a6b04f964c31
Julian Jozic <jjozic98@gmail.com>
2020-06-03 18:24:15 +0200

    Refactoring & Reset-Button
    - removed some unnecessary code and made the code a bit more clean
    - implemented a properly working reset button and changed its name to "New Quote"

--- script.js
+++ script.js
@@ -1,37 +1,27 @@
-const RANDOM_QUOTE_API_URL = 'http://api.quotable.io/random'
-
-function reset() {
-    console.log('gurke to go')
-}
-
 document.addEventListener('DOMContentLoaded', () => {
-    var words = ['hello', 'how', 'house', 'boy', 'girl', 'planet']
-    var randomWord = words[Math.floor(Math.random() * words.length)]
-    var run = 0
+    const RANDOM_QUOTE_API_URL = 'http://api.quotable.io/random'
+
+    //dom elements used to manipulate
     const quoteBox = document.getElementById('textDisplay')
-    const timerElement  = document.getElementById('timer')
+    const timerElement = document.getElementById('timer')
     const typingBox = document.getElementById('typingDisplay')
     const scoreLabel = document.getElementById('score')
     const wpmLabel = document.getElementById('wpm')
+    const resetButton = document.getElementById('reset')
 
-    renderNewQuote(true)
+    //used for wpm
+    var wordCount = 0
+
+    renderNewQuote()
 
     //reacts on each input of the typing box
     typingBox.addEventListener('input', () => {
-        let score = 0
         const arrayQuote = quoteBox.querySelectorAll('span')
         const arrayValue = typingBox.value.split('')
 
-        console.log(arrayValue)
-
-        /*for (let i = 0; i < arrayValue.length; i++) {
-            if (arrayValue[i] === text.innerHTML[i]) {
-                score++
-            }
-        }
-        scoreLabel.innerHTML = score;*/
-
+        let score = 0
         let correct = true
+
         arrayQuote.forEach((characterSpan, index) => {
             const character = arrayValue[index]
             if (character == null) {
@@ -49,9 +39,12 @@ document.addEventListener('DOMContentLoaded', () => {
                 score--
             }
         })
+
         scoreLabel.innerHTML = score
-        wpmLabel.innerHTML = getTimerTime()/score
-        if (correct) renderNewQuote()
+        if (correct) {
+            wpmLabel.innerHTML = wpmCalculation()
+            renderNewQuote()
+        }
     })
 
     function getRandomQuote() {
@@ -61,17 +54,18 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     async function renderNewQuote(newPage) {
+        wordCount = 1
         const quote = await getRandomQuote()
         quoteBox.innerHTML = ''
         quote.split('').forEach(character => {
             const characterSpan = document.createElement('span')
             characterSpan.innerText = character
             quoteBox.appendChild(characterSpan)
+            if (character === ' ') {
+                wordCount++
+            }
         })
         typingBox.value = null
-        if(!newPage){
-            return
-        }
         startTimer()
     }
 
@@ -86,5 +80,16 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function getTimerTime() {
         return Math.floor((new Date() - startTime) / 1000)
-      }
+    }
+
+    function wpmCalculation() {
+        let wpm = (wordCount / getTimerTime()) * 60
+        let roundedWpm = Math.floor(wpm*10)/10
+        return roundedWpm
+    }
+
+    reset.addEventListener('click', () => {
+        console.log('reset')
+        renderNewQuote()
+    })
 })
